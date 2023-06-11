15a24563b17dbfdf3d5a2f26284681a7d2790660
Oathan Rex <90545164+oathanrex@users.noreply.github.com>
2023-06-12 03:08:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const themeButton = document.querySelector("#theme-btn");
 const deleteButton = document.querySelector("#delete-btn");
 
 let userText = null;
-const API_KEY = "Paste your API key here"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 
 const loadDataFromLocalstorage = () => {
     // Load saved chats and theme from local storage and apply/add on the page
